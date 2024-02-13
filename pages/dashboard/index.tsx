0e4d14bc7760d84d037f1b9f11f498032617c240
Robbob24 <r.lawrence2491@gmail.com>
2024-02-13 16:52:22 -0500

    9th

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,91 +1,119 @@
 import React from "react";
-import { List, ShowButton, EditButton, useTable, DeleteButton } from "@refinedev/antd";
-import { Card, Col, Row, Space, Statistic, Table } from "antd";
-import { BaseRecord } from "@refinedev/core";
+import { useTable } from "@refinedev/antd";
+import { Card, Col, Row, Space, Statistic, Table, Timeline, Calendar } from "antd";
 import { authProvider } from "src/authProvider";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
-
+import type { Dayjs } from 'dayjs';
 
 export default function ProjectList() {
   const { tableProps } = useTable();
 
+  const onPanelChange = (value: Dayjs, mode: any) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  };
+
   return (
-    <><Card title="Dashboard"><Row gutter={16}>
-      <Col span={12}>
-        <Statistic title="Active Users" value={3} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Active Projects" value={112893} precision={2} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Active Projects" value={112893} precision={2} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Partnerships" value={112} />
-      </Col>
-    </Row>
+    <Card title="Dashboard">
+        {/*Team Card*/}
+        <Card title="Team">
+      <Row gutter={16}>
+        <Col xs={24} sm={12} md={6} lg={6}>
+          <Statistic title="Active Users" value={3} />
+        </Col>
+        <Col xs={24} sm={12} md={6} lg={6}>
+          <Statistic title="Active Projects" value={5} />
+        </Col>
+        <Col xs={24} sm={12} md={6} lg={6}>
+          <Statistic title="Pending Projects" value={20} />
+        </Col>
+        <Col xs={24} sm={12} md={6} lg={6}>
+          <Statistic title="Partnerships" value={112} />
+        </Col>
+      </Row>
+      </Card>
+      <br />
+    
+     {/*Stat Cards*/}
+      <Card title="Stats">
+      <Row gutter={16}>
+        <Col xs={24} sm={12} md={8} lg={8}>
+          <Statistic
+            title="Active"
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<ArrowUpOutlined />}
+            suffix="%"
+          />
+        </Col>
+        <Col xs={24} sm={12} md={8} lg={8}>
+          <Statistic
+            title="Active"
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: 'red' }}
+            prefix={<ArrowDownOutlined />}
+            suffix="%"
+          />
+        </Col>
+        <Col xs={24} sm={12} md={8} lg={8}>
+          <Statistic
+            title="Active"
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: 'red' }}
+            prefix={<ArrowDownOutlined />}
+            suffix="%"
+          />
+        </Col>
+      </Row>
+      </Card>
+      <br />
+
+       {/*Calendar Card*/}
+      <Card title="Calendar">
+      <Calendar onPanelChange={onPanelChange} />
+      </Card>
+      <br />
+
+
+       {/*Timeline Card*/}
+      <Card title="Timeline">
+        <Timeline>
+          <Timeline.Item>Sample</Timeline.Item>
+          <Timeline.Item>Sample</Timeline.Item>
+          <Timeline.Item>Sample</Timeline.Item>
+          <Timeline.Item>Sample</Timeline.Item>
+        </Timeline>
+      </Card>
     </Card>
-    <br/>
-
-    {/*Project Table */}
-    <Row >
-    <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-    <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{ color: '#3f8600' }}
-          prefix={<ArrowUpOutlined />}
-          suffix="%"
-        />
-    </Col>
-    <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-    <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{ color: 'red' }}
-          prefix={<ArrowDownOutlined />}
-          suffix="%"
-        />
-    </Col>
-    <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-      Col
-    </Col>
-  </Row>
-    </>
   );
-};
+}
 
-/**
- * Same check can also be done via `<Authenticated />` component.
- * But we're using a server-side check for a better UX.
- */
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-    const { authenticated, redirectTo } = await authProvider.check(context);
-  
-    const translateProps = await serverSideTranslations(context.locale ?? "en", [
-      "common",
-    ]);
-  
-    if (!authenticated) {
-      return {
-        props: {
-          ...translateProps,
-        },
-        redirect: {
-          destination: `${redirectTo}?to=${encodeURIComponent("/dashboard")}`,
-          permanent: false,
-        },
-      };
-    }
-  
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
     return {
       props: {
         ...translateProps,
       },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/dashboard")}`,
+        permanent: false,
+      },
     };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
   };
-  
+};
