4d27b3b35b9cd514013d6d02b9b6a0ea5347dc4f
Robbob24 <r.lawrence2491@gmail.com>
2024-02-12 20:55:33 -0500

    8th

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,34 +1,62 @@
-import { AntdListInferencer } from "@refinedev/inferencer/antd";
+import React from "react";
+import { List, ShowButton, EditButton, useTable, DeleteButton } from "@refinedev/antd";
+import { Avatar, Space, Table } from "antd";
+import { BaseRecord } from "@refinedev/core";
+import { authProvider } from "src/authProvider";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { authProvider } from "src/authProvider";
 
 export default function CategoryList() {
-  return <AntdListInferencer hideCodeViewerInProduction/>;
-}
+  const { tableProps } = useTable();
 
+  return (
+    <List>
+      <Table {...tableProps} rowKey="id">
+        <Table.Column dataIndex="tasks" title="Tasks" />
+        <Table.Column dataIndex="posts" title="Posts" />
+        <Table.Column
+          title="Actions"
+          dataIndex="actions"
+          render={(_, record: BaseRecord) => (
+            <Space>
+              <ShowButton hideText size="small" recordItemId={record.id} />
+              <EditButton hideText size="small" recordItemId={record.id} />
+              <DeleteButton hideText size="small" recordItemId={record.id} />
+            </Space>
+          )}
+        />
+      </Table>
+    </List>
+  );
+};
+
+/**
+ * Same check can also be done via `<Authenticated />` component.
+ * But we're using a server-side check for a better UX.
+ */
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-  const { authenticated, redirectTo } = await authProvider.check(context);
-
-  const translateProps = await serverSideTranslations(context.locale ?? "en", [
-    "common",
-  ]);
-
-  if (!authenticated) {
+    const { authenticated, redirectTo } = await authProvider.check(context);
+  
+    const translateProps = await serverSideTranslations(context.locale ?? "en", [
+      "common",
+    ]);
+  
+    if (!authenticated) {
+      return {
+        props: {
+          ...translateProps,
+        },
+        redirect: {
+          destination: `${redirectTo}?to=${encodeURIComponent("/categories")}`,
+          permanent: false,
+        },
+      };
+    }
+  
     return {
       props: {
         ...translateProps,
       },
-      redirect: {
-        destination: `${redirectTo}?to=${encodeURIComponent("/categories")}`,
-        permanent: false,
-      },
     };
-  }
-
-  return {
-    props: {
-      ...translateProps,
-    },
   };
-};
+  
