31087ec89d94c26638963090686c028ff0c9a21e
Robbob24 <r.lawrence2491@gmail.com>
2024-02-13 14:12:35 -0500

    9th

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { List, ShowButton, EditButton, useTable, DeleteButton } from "@refinedev/antd";
-import { Avatar, Space, Table } from "antd";
+import { Avatar, Input, Space, Table } from "antd";
 import { BaseRecord } from "@refinedev/core";
 import { authProvider } from "src/authProvider";
 import { GetServerSideProps } from "next";
--- pages/profiles/index.tsx
+++ pages/profiles/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { List, ShowButton, EditButton, useTable, DeleteButton } from "@refinedev/antd";
-import { Avatar, Space, Table } from "antd";
+import { Avatar, Card, Space, Table } from "antd";
 import { BaseRecord } from "@refinedev/core";
 import { authProvider } from "src/authProvider";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { UserOutlined } from "@ant-design/icons";
 
 export default function ProjectList() {
   const { tableProps } = useTable();
 
   return (
-    <List>
+    <Card title="Team Management">
       <Table {...tableProps} rowKey="id">
+        <Table.Column
+          dataIndex="avatar_url"
+          title="User"
+          render={(avatarUrl) => (
+            <Avatar shape="circle" src={avatarUrl} size={50} icon={<UserOutlined />} />
+          )}
+        />
         <Table.Column dataIndex="username" title="Username" />
         <Table.Column dataIndex="role" title="Role" />
         <Table.Column dataIndex="full_name" title="Full Name" />
@@ -23,42 +31,40 @@ export default function ProjectList() {
             <Space>
               <ShowButton hideText size="small" recordItemId={record.id} />
               <EditButton hideText size="small" recordItemId={record.id} />
-              <DeleteButton hideText size="small" recordItemId={record.id} />
             </Space>
           )}
         />
       </Table>
-    </List>
+      </Card>
   );
-};
+}
 
 /**
  * Same check can also be done via `<Authenticated />` component.
  * But we're using a server-side check for a better UX.
  */
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-    const { authenticated, redirectTo } = await authProvider.check(context);
-  
-    const translateProps = await serverSideTranslations(context.locale ?? "en", [
-      "common",
-    ]);
-  
-    if (!authenticated) {
-      return {
-        props: {
-          ...translateProps,
-        },
-        redirect: {
-          destination: `${redirectTo}?to=${encodeURIComponent("/profiles")}`,
-          permanent: false,
-        },
-      };
-    }
-  
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
     return {
       props: {
         ...translateProps,
       },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/profiles")}`,
+        permanent: false,
+      },
     };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
   };
-  
+};
