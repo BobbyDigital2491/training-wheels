0318c194c5a4d5e7b9ee42cef3ecc759868b87e4
Robbob24 <r.lawrence2491@gmail.com>
2024-02-15 00:12:22 -0500

    21st

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTable } from "@refinedev/antd";
-import { Card, Col, Row, Space, Statistic, Table, Timeline, Calendar, Checkbox, Form, Progress } from "antd";
+import { Card, Col, Row, Space, Statistic, Table, Timeline, Calendar, Checkbox, Form, Progress, Carousel, Tooltip } from "antd";
 import { authProvider } from "src/authProvider";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
@@ -8,6 +8,9 @@ import { AimOutlined, ArrowDownOutlined, ArrowUpOutlined, DashboardOutlined, Fal
 import type { Dayjs } from 'dayjs';
 import TimelineItem from "antd/lib/timeline/TimelineItem";
 import { useState } from "react";
+import { Gauge, Line } from "@ant-design/charts";
+import LineGraph from "@components/LineGraph";
+
 
 export default function ProjectList() {
   const { tableProps } = useTable();
@@ -16,149 +19,157 @@ export default function ProjectList() {
     console.log(value.format('YYYY-MM-DD'), mode);
   };
 
+  const contentStyle: React.CSSProperties = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#000000',
+  };
+
   
 
   return (
-    <Card title="Dashboard" >
+    <>
+      
+    {/*shhs*/}
+    <Card title="Dashboard">
         {/*Team Card*/}
         <Card title="General">
-      <Row gutter={16}>
-        <Col xs={24} sm={12} md={6} lg={6}>
-          <Statistic title="Active Users" value={3} prefix={<UserOutlined />} />
-        </Col>
-        <Col xs={24} sm={12} md={6} lg={6}>
-          <Statistic title="Active Projects" value={5}  prefix={<AimOutlined/>} />
-        </Col>
-        <Col xs={24} sm={12} md={6} lg={6}>
-          <Statistic title="Pending Projects" value={20}  prefix={<ProjectOutlined/>} />
-        </Col>
-        <Col xs={24} sm={12} md={6} lg={6}>
-          <Statistic title="Partnerships" value={112} prefix={<LikeOutlined/>} />
-        </Col>
-      </Row>
-      </Card>
-      <br />
-    
-     {/*Stat Cards*/}
-      <Card title="Stats">
-      <Row gutter={16}>
-        <Col xs={24} sm={12} md={8} lg={8}>
-          <Statistic
-            title="Completed Projects"
-            value={11.28}
-            precision={2}
-            valueStyle={{ color: '#3f8600' }}
-            prefix={<RiseOutlined />}
-            suffix="%"
-          />
-        </Col>
-        <Col xs={24} sm={12} md={8} lg={8}>
-          <Statistic
-            title="Pending Projects"
-            value={11.28}
-            precision={2}
-            valueStyle={{ color: 'red' }}
-            prefix={<FallOutlined />}
-            suffix="%"
-          />
-        </Col>
-        <Col xs={24} sm={12} md={8} lg={8}>
-          <Statistic
-            title="Sales"
-            value={50}
-            precision={2}
-            valueStyle={{ color: 'red' }}
-            prefix={<ArrowDownOutlined />}
-            suffix="%"
-          />
-        </Col>
-       
-
-      {/*End of Stats*/}
-      </Row>
-      </Card>
-      <br />
-      {/*End of Stats*/}
-
-       {/*Calendar Card*/}
-      <Card title="Calendar">
-      <Calendar onPanelChange={onPanelChange} />
-      </Card>
-      <br />
-
-
-       {/*Timeline Card*/}
-       <Card title="Timeline">
-      <Row gutter={16}>
-        <Col xs={24} sm={12} md={8} lg={8}>
-        <h1>Past</h1>
-        <Timeline >
-          <TimelineItem>Mercedez Meeting - Feb 15 2024</TimelineItem>
-          <TimelineItem>Augmentique Meeting - Feb 14 2024</TimelineItem>
-          <TimelineItem>Bell Meeting - Feb 27 2024</TimelineItem>
-            </Timeline>
-        </Col>
-        
-        <Col xs={24} sm={12} md={8} lg={8}>
-        <h1>Upcoming</h1>
-        <Timeline>
-          <TimelineItem>Mercedez Meeting - Feb 15 2024</TimelineItem>
-          <TimelineItem>Augmentique Meeting - Feb 14 2024</TimelineItem>
-          <TimelineItem>Bell Meeting - Feb 27 2024</TimelineItem>
-            </Timeline>
-        </Col>
-        
-        <Col xs={24} sm={12} md={8} lg={8}>  
-        <h1>Next Month</h1>
-        <Timeline >
-          <TimelineItem>Mercedez Meeting - Feb 15 2024</TimelineItem>
-          <TimelineItem>Augmentique Meeting - Feb 14 2024</TimelineItem>
-          <TimelineItem>Bell Meeting - Feb 27 2024</TimelineItem>
-            </Timeline>
-        </Col>
-      </Row>
-      </Card>
-      <br/>
-
-      {/*To Do List*/}
-      
-      <Card title="To Do List">
-        <Row gutter={16}>
-      <Col xs={24} sm={12} md={8} lg={8}>
-      <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> Bell Event Flyer
-      </Form.Item>
-      <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> V2 Magazine
-      </Form.Item>
-      <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> Budget Planning
-      </Form.Item>
-      </Col>
-
-      <Col xs={24} sm={12} md={8} lg={8}>
-      <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> Augmentique Test Flight
-      </Form.Item>
-      <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> V2 Magazine
-      </Form.Item>
-     </Col>
-
-     <Col xs={24} sm={12} md={8} lg={8}>
-     <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> Budget Planning
-      </Form.Item>
-      <Form.Item name="fieldA" valuePropName="checked" >
-      <Checkbox name=""/> Budget Planning
-      </Form.Item>
-     </Col>
-
-     </Row>
+          <Row gutter={16}>
+            <Col xs={24} sm={12} md={6} lg={6}>
+              <Statistic title="Active Users" value={3} prefix={<UserOutlined />} />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={6}>
+              <Statistic title="Active Projects" value={5} prefix={<AimOutlined />} />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={6}>
+              <Statistic title="Pending Projects" value={20} prefix={<ProjectOutlined />} />
+            </Col>
+            <Col xs={24} sm={12} md={6} lg={6}>
+              <Statistic title="Partnerships" value={112} prefix={<LikeOutlined />} />
+            </Col>
+          </Row>
         </Card>
-    
-     
-    </Card>
+        <br />
+
+        {/*Stat Cards*/}
+        <Card title="Stats">
+          <Row gutter={16}>
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <Statistic
+                title="Completed Projects"
+                value={11.28}
+                precision={2}
+                valueStyle={{ color: '#3f8600' }}
+                prefix={<RiseOutlined />}
+                suffix="%" />
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <Statistic
+                title="Pending Projects"
+                value={11.28}
+                precision={2}
+                valueStyle={{ color: 'red' }}
+                prefix={<FallOutlined />}
+                suffix="%" />
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <Statistic
+                title="Sales"
+                value={50}
+                precision={2}
+                valueStyle={{ color: 'red' }}
+                prefix={<ArrowDownOutlined />}
+                suffix="%" />
+            </Col>
+
+
+            {/*End of Stats*/}
+          </Row>
+        </Card>
+        <br />
+        {/*End of Stats*/}
+
+        {/*Calendar Card*/}
+        <Card title="Calendar">
+          <Calendar onPanelChange={onPanelChange} />
+        </Card>
+        <br />
+
+
+        {/*Timeline Card*/}
+        <Card title="Timeline">
+          <Row gutter={16}>
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <h1>Past</h1>
+              <Timeline>
+                <TimelineItem>Mercedez Meeting - Feb 15 2024</TimelineItem>
+                <TimelineItem>Augmentique Meeting - Feb 14 2024</TimelineItem>
+                <TimelineItem>Bell Meeting - Feb 27 2024</TimelineItem>
+              </Timeline>
+            </Col>
+
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <h1>Upcoming</h1>
+              <Timeline>
+                <TimelineItem>Mercedez Meeting - Feb 15 2024</TimelineItem>
+                <TimelineItem>Augmentique Meeting - Feb 14 2024</TimelineItem>
+                <TimelineItem>Bell Meeting - Feb 27 2024</TimelineItem>
+              </Timeline>
+            </Col>
+
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <h1>Next Month</h1>
+              <Timeline>
+                <TimelineItem>Mercedez Meeting - Feb 15 2024</TimelineItem>
+                <TimelineItem>Augmentique Meeting - Feb 14 2024</TimelineItem>
+                <TimelineItem>Bell Meeting - Feb 27 2024</TimelineItem>
+              </Timeline>
+            </Col>
+          </Row>
+        </Card>
+        <br />
+
+        {/*To Do List*/}
+
+        <Card title="To Do List">
+          <Row gutter={16}>
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> Bell Event Flyer
+              </Form.Item>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> V2 Magazine
+              </Form.Item>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> Budget Planning
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> Augmentique Test Flight
+              </Form.Item>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> V2 Magazine
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={12} md={8} lg={8}>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> Budget Planning
+              </Form.Item>
+              <Form.Item name="fieldA" valuePropName="checked">
+                <Checkbox name="" /> Budget Planning
+              </Form.Item>
+            </Col>
+
+          </Row>
+        </Card>
+
+
+      </Card></>
   );
 }
 
--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,34 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { Line } from '@ant-design/plots';
+import React, { useState, useEffect, useRef } from 'react';
+import { Line } from '@ant-design/charts';
+import { ReactI18NextChild } from 'react-i18next';
 
-export const LineGraph = () => {
-  const [data, setData] = useState([]);
+const DemoLine: React.FC = () => {
+  const data = [
+    {
+      year: '1991',
+      value: 3,
+    },
+    {
+      year: '1992',
+      value: 4,
+    },
+    {
+      year: '1993',
+      value: 3.5,
+    },
+    {
+      year: '1994',
+      value: 5,
+    },
+    {
+      year: '1995',
+      value: 4.9,
+    },
+    {
+      year: '1996',
+      value: 6,
+    },
+    {
+      year: '1997',
+      value: 7,
+    },
+    {
+      year: '1998',
+      value: 9,
+    },
+    {
+      year: '1999',
+      value: 13,
+    },
+  ];
 
-  useEffect(() => {
-    asyncFetch();
-  }, []);
-
-  const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log('fetch data failed', error);
-      });
-  };
   const config = {
     data,
-    padding: 'auto',
-    xField: 'Date',
-    yField: 'scales',
-    xAxis: {
-      // type: 'timeCat',
-      tickCount: 5,
+    yField: 'value',
+    xField: 'year',
+    tooltip: {
+      customContent: (title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | Iterable<ReactI18NextChild> | null | undefined, items: any[]) => {
+        return (
+          <>
+            <h5 style={{ marginTop: 16 }}>{title}</h5>
+            <ul style={{ paddingLeft: 0 }}>
+              {items?.map((item, index) => {
+                const { name, value, color } = item;
+                return (
+                  <li
+                    key={item.year}
+                    className="g2-tooltip-list-item"
+                    data-index={index}
+                    style={{ marginBottom: 4, display: 'flex', alignItems: 'center' }}
+                  >
+                    <span className="g2-tooltip-marker" style={{ backgroundColor: color }}></span>
+                    <span
+                      style={{ display: 'inline-flex', flex: 1, justifyContent: 'space-between' }}
+                    >
+                      <span style={{ marginRight: 16 }}>{name}:</span>
+                      <span className="g2-tooltip-list-item-value">{value}</span>
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </>
+        );
+      },
+    },
+    point: {
+      size: 5,
+      shape: 'diamond',
+      style: {
+        fill: 'white',
+        stroke: '#2593fc',
+        lineWidth: 2,
+      },
     },
   };
 
   return <Line {...config} />;
 };
 
-ReactDOM.render(<LineGraph />, document.getElementById('container'));
+export default DemoLine;
