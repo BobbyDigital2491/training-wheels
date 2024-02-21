60aadf1c143566a210b57e3dd0268616f2e5b83b
Robbob24 <r.lawrence2491@gmail.com>
2024-02-21 13:22:56 -0500

    36th

---
+++ pages/SMP/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { List, ShowButton, EditButton, useTable, DeleteButton } from "@refinedev/antd";
+import { Card, Space, Table } from "antd";
+import { BaseRecord } from "@refinedev/core";
+import { authProvider } from "src/authProvider";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function SMPList() {
+  const { tableProps } = useTable();
+
+  return (
+    <Card>
+    <List>
+      <Table {...tableProps} rowKey="id">
+        
+        <Table.Column dataIndex="no" title="Title" />
+        <Table.Column dataIndex="content" title="Content" />
+        <Table.Column dataIndex="created_at" title="Created at" />
+        <Table.Column dataIndex="work_element" title="Production Lines" />
+        <Table.Column
+          title="Actions"
+          dataIndex="actions"
+          render={(_, record: BaseRecord) => (
+            <Space>
+              <ShowButton hideText size="small" recordItemId={record.id} />
+              <EditButton hideText size="small" recordItemId={record.id} />
+              <DeleteButton hideText size="small" recordItemId={record.id} />
+            </Space>
+          )}
+        />
+      </Table>
+    </List>
+    </Card>
+  );
+};
+
+/**
+ * Same check can also be done via `<Authenticated />` component.
+ * But we're using a server-side check for a better UX.
+ */
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+    const { authenticated, redirectTo } = await authProvider.check(context);
+  
+    const translateProps = await serverSideTranslations(context.locale ?? "en", [
+      "common",
+    ]);
+  
+    if (!authenticated) {
+      return {
+        props: {
+          ...translateProps,
+        },
+        redirect: {
+          destination: `${redirectTo}?to=${encodeURIComponent("/Standard Method & Procedure (SMP)")}`,
+          permanent: false,
+        },
+      };
+    }
+  
+    return {
+      props: {
+        ...translateProps,
+      },
+    };
+  };
+  
