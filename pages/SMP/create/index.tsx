4f12b5d8bf25bcef818f5be492e5942125486251
Robbob24 <r.lawrence2491@gmail.com>
2024-02-21 13:57:14 -0500

    40th

--- pages/SMP/create/index.tsx
+++ pages/SMP/create/index.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
 
-export default function SMPCreate() {
+export default function ProjectCreate() {
   return <AntdCreateInferencer hideCodeViewerInProduction/>;
 }
 
